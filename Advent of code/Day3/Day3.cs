c9ccf210a4773f3d330388c1cbf6a397a66d8289
Mjurtas <marten.bendroth@gmail.com>
2020-12-03 23:18:02 +0100

    Day 3 passed! Hard time finding out it was (ulong) that was missing in pt 2. First try with 2D Arrays aswell.
    Good times.

---
+++ Advent of code/Day3/Day3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Advent_of_code.Day3
+{
+    class Day3
+    {
+
+        public string[] Layout { get; set; }
+        public int TreeCountPartOne { get; set; }
+        public ulong TreeCountPartTwo { get; set; }
+
+        public Day3()
+        {
+            Layout = ParseDataToStringArray();
+            TreeCountPartOne = SolutionPartOne(Layout, 1, 3);
+            TreeCountPartTwo = SolutionPartTwo();
+
+        }
+       
+        private string[] ParseDataToStringArray()
+        {
+            string data = File.ReadAllText("C:\\Users\\Marten\\source\\repos\\Advent of code\\Advent of code\\Day3\\slopelayout.txt");
+            return data.Split(Environment.NewLine);
+        }
+
+        private int SolutionPartOne(string[] slope, int down, int right)
+        {
+            
+            int trees = 0;
+            int column = 0;
+            int width = slope[0].Length;
+
+            for (int i = down; i < slope.Length; i += down)
+            {
+                column += right;
+                if (column >= width) column %= width;
+                
+                if (slope[i][column] == '#')  trees++;
+                
+            }
+            return trees;
+        }
+
+        private ulong SolutionPartTwo()
+        {
+            int[,] slopes = new int[,] { { 1, 1 }, { 1, 3 }, { 1, 5 }, { 1, 7 }, { 2, 1 } };
+            ulong result = 1;
+            for (int i = 0; i < slopes.Length / 2; i++)
+            {
+                result *= (ulong)SolutionPartOne(Layout, slopes[i, 0], slopes[i, 1]);
+
+            };
+            
+            return result;
+        }
+            
+            
+
+    }
+}
--- Advent of code/Program.cs
+++ Advent of code/Program.cs
@@ -10,12 +10,19 @@ namespace Advent_of_code
         static void Main(string[] args)
         {
             Day1.Day1 day1 = new Day1.Day1();
-            Console.WriteLine("1th December Pt.1 = " + day1.SolutionPartOne);
-            Console.WriteLine("1th December Pt.2 = " + day1.SolutionPartTwo);
-
+            Console.WriteLine("1th December Pt.1 = " + day1.SolutionPartOne + " is the product of the two numbers.");
+            Console.WriteLine("1th December Pt.2 = " + day1.SolutionPartTwo + " is the product of the three numbers.");
+          
             Day2.Day2 day2 = new Day2.Day2();
-            Console.WriteLine("2nd December Pt.1   " + day2.NumberOfValidPasswordsPartOne);
-            Console.WriteLine("2nd December Pt.2   " + day2.NumberOfValidPasswordsPartTwo);
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("2nd December Pt.1   " + day2.NumberOfValidPasswordsPartOne + " valid passwords.");
+            Console.WriteLine("2nd December Pt.2   " + day2.NumberOfValidPasswordsPartTwo + " valid passwords.");
+            Console.WriteLine("------------------------------");
+
+            Day3.Day3 day3 = new Day3.Day3();
+            Console.WriteLine("3rd December Pt.1    "+ day3.TreeCountPartOne + " trees were hit in the slope of 1 down and 3 right.");
+            Console.WriteLine("3rd December Pt.2    "+ day3.TreeCountPartTwo + " trees were hit when multiplying all of the given slopes.");
+            Console.WriteLine("------------------------------");
         }
     }
 }
